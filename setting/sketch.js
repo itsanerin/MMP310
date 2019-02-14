49f233e051709dfb4d9d8d71c378b35cbc1dc01c
itsanerin <31420768+itsanerin@users.noreply.github.com>
2019-02-14 14:21:51 -0500

    split into functions

--- setting/sketch.js
+++ setting/sketch.js
@@ -3,24 +3,6 @@
 //adding 3 settings
 //by erin brady
 
-//bird variables
-var birdX = 425; //250
-var birdY = 120; //150
-var birdX2 = birdX - 70; //180
-var birdY2 = birdY + 33; //183
-var birdX3 = birdX2 - 100; //80
-var birdY3 = birdY2 + 50; //233
-var bHeadWidth = 50; //50
-var bHeadHeight = bHeadWidth - 15; //35
-var eyeSize = 6;
-var bFootSize = 30;
-
-//squirrel variables
-var squirrelX = 750;
-var squirrelY = 300; //135
-var earSize = 20; //20
-var squirrelWidth = 65;
-var squirrelHeight = 130;
 
 //misc variables
 var dayColor = '#69cae0';
@@ -43,7 +25,30 @@ function draw() {
     image(img, 0, 0); //backyard background
 
     // bird
-    fill('#868686');
+    bird();
+
+    //2nd character squirrel
+    squirrel();
+
+    //night time darkness
+    fill(darkness);
+    rect(0, 0, 900, 447);
+}
+
+function bird() {
+    //variables
+    var birdX = 425; //250
+    var birdY = 120; //150
+    var birdX2 = birdX - 70; //180
+    var birdY2 = birdY + 33; //183
+    var birdX3 = birdX2 - 100; //80
+    var birdY3 = birdY2 + 50; //233
+    var bHeadWidth = 50; //50
+    var bHeadHeight = bHeadWidth - 15; //35
+    var eyeSize = 6;
+    var bFootSize = 30;
+
+    fill('#868686'); //gray
     strokeWeight(1);
 
     //bird body
@@ -52,19 +57,28 @@ function draw() {
     bezierVertex(birdX - 15, birdY + 130, birdX2, birdY2, birdX3, birdY3);
     endShape(CLOSE)
 
-    ellipse(birdX - 15, birdY + 10, bHeadWidth, bHeadHeight); // bird head
+    // bird head
+    ellipse(birdX - 15, birdY + 10, bHeadWidth, bHeadHeight);
 
     // bird eye
     fill('black');
     ellipse(birdX - 5, birdY + 5, eyeSize, eyeSize);
 
-    fill('#f4f43d');
+    fill('#f4f43d'); //yellow
     triangle(birdX + 5, birdY + 20, birdX + 10, birdY + 10, birdX + 25, birdY + 20); // bird beak
     arc(birdX - 45, birdY + 75, bFootSize, bFootSize, -PI, 0, CHORD); //bird foot
+}
 
+function squirrel() {
 
-    //2nd character squirrel
-    fill('#874934');
+    // variables
+    var squirrelX = 750;
+    var squirrelY = 300; //135
+    var earSize = 20; //20
+    var squirrelWidth = 65;
+    var squirrelHeight = 130;
+
+    fill('#874934'); // dark brown
     rect(squirrelX, squirrelY, squirrelWidth - 20, squirrelHeight, 15); //squirrel tail
     arc(squirrelX - 25, squirrelY + 130, squirrelWidth + 5, squirrelHeight + 50, -PI, 0, CHORD); // squirrel body
     ellipse(squirrelX - 55, squirrelY + 20, earSize, earSize); //squirrel left ear
@@ -72,7 +86,7 @@ function draw() {
     ellipse(squirrelX - 45, squirrelY + 40, earSize * 3, earSize + 25); //squirrel head
 
     //squirrel tummy
-    fill('#e1bb80');
+    fill('#e1bb80'); //light brown
     arc(squirrelX - 40, squirrelY + 125, squirrelWidth - 35, squirrelHeight - 35, -PI, 0, CHORD);
 
     //squirrel tooth
@@ -82,11 +96,6 @@ function draw() {
     //squirrel eye
     fill('black');
     ellipse(squirrelX - 60, squirrelY + 35, eyeSize, eyeSize);
-
-
-    //night time darkness
-    fill(darkness);
-    rect(0, 0, 900, 447);
 }
 
 function mouseClicked() {
